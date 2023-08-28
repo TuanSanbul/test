0bf55e6c828ba6d4eb5381ff241c1af41a3eb257
TuanSanbul <tuan.hd@sanbul.net>
2023-08-28 22:43:54 +0700

    fix: test3

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,4 +1,7 @@
+import { HealthService } from '@lib/utils/modules';
 import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AuthServiceController {}
+export class AuthServiceController {
+  constructor(private readonly healthService: HealthService) {}
+}
