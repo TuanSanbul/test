e217354e757f68fcd7cfd5ee0a06e0a38d86dc9b
TuanSanbul <tuan.hd@sanbul.net>
2023-08-29 10:21:23 +0700

    feat: test2

--- apps/admin-gateway/src/communicate/connection-status/connection-status.controller.ts
+++ apps/admin-gateway/src/communicate/connection-status/connection-status.controller.ts
@@ -1,17 +1,8 @@
-import { ServiceName } from '@lib/common/enums';
-import { IPatternMessage } from '@lib/common/interfaces/services';
 import { BaseGatewayController } from '@lib/core/base';
-import { ServiceProviderBuilder } from '@lib/core/message-handler';
-import { TimeFilterDto } from '@lib/utils/validation-pipe';
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { ConnStatusDto } from './dto';
 
 @Controller('connection-status')
 @ApiTags('Communicate Service')
 @ApiBearerAuth()
-export class ConnectionStatusController extends BaseGatewayController {
-  constructor(private readonly serviceClient: ServiceProviderBuilder) {
-    super(ConnectionStatusController.name, ServiceName.COMMUNICATE_SERVICE);
-  }
-}
+export class ConnectionStatusController extends BaseGatewayController {}
