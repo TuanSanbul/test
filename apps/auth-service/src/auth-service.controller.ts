472784ec90c857b23badae0ca553dcae15d73e27
TuanSanbul <tuan.hd@sanbul.net>
2023-08-29 09:17:29 +0700

    feat: test10

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,8 +1,5 @@
-import { HealthService } from '@lib/utils/modules';
 import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AuthServiceController {
-  constructor(private readonly healthService: HealthService) {}
-
 }
